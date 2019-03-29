2aca12d2e9a6cd7f391bc8dcfcddc25ea5ce243c
Deyan Kamburov <dkamburov@infragistics.com>
2019-03-29 14:41:18 +0200

    Fix lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IgxNavigationDrawerModule, IgxNavbarModule, IgxLayoutModule, IgxRippleModule, IgxCardModule, IgxAvatarComponent, IgxAvatarModule } from 'igniteui-angular';
+import { IgxNavigationDrawerModule,
+    IgxNavbarModule,
+    IgxLayoutModule,
+    IgxRippleModule,
+    IgxCardModule,
+    IgxAvatarModule } from 'igniteui-angular';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
